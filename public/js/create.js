552dbafa2922f807082ae7fadd41ccdfbc4af1ff
Abdul Rehman <abdul-rehman@myseneca.ca>
2018-12-11 18:22:36 -0600

    Added a way to add new ingredients

--- public/js/create.js
+++ public/js/create.js
@@ -5,4 +5,14 @@ $(function() {
    $("#rDiff").mousemove(function () {
       $("#text").text($("#rDiff").val())
   });
+
+  $('#newIng').click(function() {
+
+   const newRow = `<div class="row">
+                  <input class="col-md-3 offset-1 form-control" name="ingredients" style="margin: 1%;" type="text" placeholder="ingredient">
+                  </div>`;
+
+     $(".newIngredient").append(newRow);
+   });
+
 });
